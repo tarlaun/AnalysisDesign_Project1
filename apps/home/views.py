53d1dffb1005daa295b812272f4fda022b475499
bahar <baharkh127@gmail.com>
2021-11-13 00:50:05 +0330

    init ads list in user dashboard

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,13 +8,30 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from .models import Ad
+import datetime
 
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
+    # here we should take all ads related to the user and pass it to html template
 
-    html_template = loader.get_template('home/index.html')
+    ad1 = Ad(first_name="user1", last_name="kh1", phone_number="09136875776",
+                 address="khaghani", start_time=datetime.datetime.now(), end_time=datetime.datetime.now(), 
+                 service_type='1', sex="woman")
+
+    ad2 = Ad(first_name="user2", last_name="kh2", phone_number="09136775776",
+                 address="khaghani", start_time=datetime.datetime.now(), end_time=datetime.datetime.now(), 
+                 service_type='3', sex="man")
+
+    ad3 = Ad(first_name="user3", last_name="kh3", phone_number="09136675776",
+                 address="khaghani", start_time=datetime.datetime.now(), end_time=datetime.datetime.now(), 
+                 service_type='2', sex="woman")
+
+    ads = [ad1, ad2, ad3]
+    context = {'ads': ads}
+
+    html_template = loader.get_template('home/ads.html')
     return HttpResponse(html_template.render(context, request))
 
 
